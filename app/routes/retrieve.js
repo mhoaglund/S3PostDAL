f5a1ee32b11d58306c1ee46c8a5a84d9401302b3
mhoaglund <maxwell.hoaglund@gmail.com>
2018-01-13 20:50:24 -0600

    cleanup

--- app/routes/retrieve.js
+++ app/routes/retrieve.js
@@ -4,34 +4,15 @@ var UUID = require('uuid')
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
-/* GET listing of bucket contents. */
-// router.get('/', require('connect-ensure-login').ensureLoggedIn(), function(req, res, next) {
-//     if(req.query.itemid){
-//         var params = {
-//             Bucket: _dp.location,
-//             Kwy: req.query.itemid
-//         }
-//         s3.getObject(params, function(err, data){
-//             if(err){
-//                 console.log(err)
-//                 res.send('Item not found.')
-//             }
-//             else{
-//                 res.send(JSON.stringify(data))
-//             }
-//         })
-//     }
-// });
-
 //TODO cache stuff, cache newest timestamp etc.
 router.get('/', function(req, res, next) {
     if(!req.query.latest){
         _dp._get_items(req.targetprop, '>', req.compval, function(data){
-            res.send(JSON.stringify(data))
+            res.send(data)
         })
     } else{
         _dp._get_latest(true, function(data){
-            res.send(JSON.stringify(data))
+            res.send(data)
         })
     }
 })
